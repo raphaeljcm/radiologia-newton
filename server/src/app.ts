1f93208a27beba8b433c7ee8e673ebdc57fb8a4a
Raphael Marques <raphaeljcmarques@gmail.com>
2023-10-29 20:37:26 -0300

    chore: small update on server

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,18 +4,24 @@ import { router } from './router';
 
 const app = express();
 
-app.use(router);
 app.use(express.json());
+app.use(router);
 
-app.listen(process.env.PORT, () => {
-  connection.connect(err => {
-    if (err) {
-      console.log('Error connecting to database');
-      throw new Error(err.message);
-    }
+app.listen(
+  {
+    port: process.env.PORT,
+    host: '0.0.0.0',
+  },
+  () => {
+    connection.connect(err => {
+      if (err) {
+        console.log('Error connecting to database');
+        throw new Error(err.message);
+      }
 
-    console.log('Connected to database');
-  });
+      console.log('Connected to database');
+    });
 
-  console.log(`Server listening on port ${process.env.PORT}`);
-});
+    console.log(`Server listening on port ${process.env.PORT}`);
+  },
+);
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { login } from './controllers/login';
 
 export const router = Router();
 
-router.get('/', () => console.log('Hello world!'));
+router.get('/', (req: Request, res: Response) => res.send('Hello World'));
 
 router.post('/login', login);

82348fef14713779f9a800fac0bfae1b286c1b83
Raphael Marques <raphaeljcmarques@gmail.com>
2023-10-29 20:58:00 -0300

    refactor: remove host config

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,21 +7,15 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(
-  {
-    port: process.env.PORT,
-    host: '0.0.0.0',
-  },
-  () => {
-    connection.connect(err => {
-      if (err) {
-        console.log('Error connecting to database');
-        throw new Error(err.message);
-      }
+app.listen(process.env.PORT, () => {
+  connection.connect(err => {
+    if (err) {
+      console.log('Error connecting to database');
+      throw new Error(err.message);
+    }
 
-      console.log('Connected to database');
-    });
+    console.log('Connected to database');
+  });
 
-    console.log(`Server listening on port ${process.env.PORT}`);
-  },
-);
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
